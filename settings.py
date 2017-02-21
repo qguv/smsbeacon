076325e625348928acd2f4ecc08fa5ee8171f343
Quint Guvernator <quint@guvernator.net>
2017-02-21 00:12:39 -0500

    increase and describe veto_delay

--- settings.py
+++ settings.py
@@ -3,8 +3,12 @@ plivo_id        = "your_plivo_auth_id"
 plivo_token     = "your_plivo_auth_token"
 plivo_number    = "18665551234"
 endpoint        = "http://beacon.example.com/" # with trailing slash
+
+# your messages will clear the queue between (queue_interval * (veto_delay - 1))
+# and (queue_interval * veto_delay) seconds
+
 queue_interval  = 60 # seconds
-veto_delay      = 2 # queue intervals
+veto_delay      = 3 # queue intervals to keep messages in the queue
 
 vetoers = {
     "18885552468": "Quint",
