1f03deef1aba9141feac6c031aaa4ab710e1a355
Quint Guvernator <quint@guvernator.net>
2017-02-20 23:12:46 -0500

    wait two queue intervals by default

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ plivo_token     = "your_plivo_auth_token"
 plivo_number    = "18665551234"
 endpoint        = "http://beacon.example.com/" # with trailing slash
 queue_interval  = 60 # seconds
-veto_delay      = 4 # queue intervals
+veto_delay      = 2 # queue intervals
 
 vetoers = {
     "18885552468": "Quint",
