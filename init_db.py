bf94cc2e44a583b5152fac72b183433ad42064cc
Quint Guvernator <quint@guvernator.net>
2017-09-05 21:57:53 -0400

    get UserType from db.py to avoid instantiating app

--- init_db.py
+++ init_db.py
@@ -4,8 +4,7 @@
 import os, sys; sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "./vendored"))
 
 import config
-from app import UserType
-from db import Database
+from db import Database, UserType
 from utils import random_token
 from datetime import datetime
 
