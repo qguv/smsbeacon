c2caff7f12cb6d569d2e37001ff0d507a131acf8
Quint Guvernator <quint@guvernator.net>
2017-09-05 21:00:46 -0400

    ugly hack to call process regularly; should be using SQS instead

---
+++ call_process.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Incredibly ugly hack to call process route regularly. TODO: use SQS instead'''
+
+def handler():
+    import requests # external
+    import config   # internal
+
+    protocol = "https" if config.https else "http"
+    port = '' if config.port == 80 else ':{}'.format(port)
+
+    url = protocol + '://' + config.public_url + port + '/p/' + config.processing_key
+
+    return requests.get(url)
