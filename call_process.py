8f3972bf076f4002ed02f31c52dd512d9e78d5e4
Quint Guvernator <quint@guvernator.net>
2017-09-09 13:04:30 -0400

    call_process uses native urllib.request

--- call_process.py
+++ call_process.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 '''Incredibly ugly hack to call process route regularly. TODO: use SQS instead'''
 
-def handler(*args):
-    import requests # external
-    import config   # internal
+import urllib.request, urllib.error
+import config # internal
 
+def handler(*args):
     protocol = "https" if config.https else "http"
     port = '' if config.port == 80 else ':{}'.format(port)
 
     url = protocol + '://' + config.public_url + port + '/p/' + config.processing_key
 
-    return requests.get(url)
+    try:
+        urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+        print("HTTP Error {}: {}".format(*e.reason))
